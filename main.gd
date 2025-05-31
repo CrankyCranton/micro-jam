6a4cecc8d47dfce13a90178431752f711395b520
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-06-01 00:38:46 +0600

    k

--- main.gd
+++ main.gd
@@ -5,4 +5,4 @@ func _ready() -> void:
 	const DIALOGUE := preload("res://dialogue/main.dialogue")
 	DialogueManager.show_dialogue_balloon(DIALOGUE)
 	await DialogueManager.dialogue_ended
-	get_tree().quit()#get_tree().change_scene_to_file()
+	#get_tree().quit()#get_tree().change_scene_to_file()
