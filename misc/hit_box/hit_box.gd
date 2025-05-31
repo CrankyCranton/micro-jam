315fbb9c434ecb786ab3647dd3545e8267d15ae0
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-05-31 00:55:24 -0400

    Hit box immunity

--- misc/hit_box/hit_box.gd
+++ misc/hit_box/hit_box.gd
@@ -3,6 +3,15 @@ class_name HitBox extends Area2D
 
 signal damage_taken(damage: int)
 
+@onready var collision_shape: CollisionShape2D = $CollisionShape
+@onready var immunity_time: Timer = $ImmunityTime
+
 
 func take_damage(damage: int) -> void:
 	damage_taken.emit(damage)
+	collision_shape.set_deferred(&"disabled", true)
+	immunity_time.start()
+
+
+func _on_immunity_time_timeout() -> void:
+	collision_shape.set_deferred(&"disabled", false)
