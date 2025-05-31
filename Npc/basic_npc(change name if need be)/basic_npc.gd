7219e65fe50776ab285e37c30fc50c3044cc724a
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-05-31 23:29:41 +0600

    some npc and chain

---
+++ Npc/basic_npc(change name if need be)/basic_npc.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+@onready var player:Player = get_tree().get_first_node_in_group("player")
+@onready var nav_agent:NavigationAgent2D = $NavigationAgent2D
+
+@export var speed:int
+@export var acceleration:int
+
+enum states {
+	idle,
+	walk_around,
+	minion_idle,
+	minion_move,
+	minion_attack
+}
+
+var STATE:states = states.minion_move
+
+
+func _physics_process(delta: float) -> void:
+	match STATE:
+		states.idle:
+			idle()
+		states.walk_around:
+			pass
+		states.minion_move:
+			minion_move()
+		states.minion_attack:
+			minion_attack()
+		states.minion_idle:
+			minion_idle()
+	move_and_slide()
+
+func idle():
+	pass
+
+func minion_move():
+	if player:
+		var direction = Vector2()
+
+		nav_agent.target_position = player.global_position
+		direction = nav_agent.get_next_path_position() - global_position
+		direction = direction.normalized()
+
+		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(direction * speed,acceleration)
+
+		if nav_agent.distance_to_target() < 50:
+			print("hey")
+			STATE = states.minion_idle
+
+func minion_attack():
+	pass
+
+func minion_idle():
+	velocity = Vector2.ZERO
+	nav_agent.target_position = player.global_position
+	if nav_agent.distance_to_target() > 50:
+		STATE = states.minion_move
