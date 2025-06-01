246d0d0dcbd41d8b4fbdaa7328bc38ceb7ea09a3
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-06-01 00:19:45 -0400

    Restore ability input to player

--- player/abilities/ability.gd
+++ player/abilities/ability.gd
@@ -3,19 +3,7 @@ class_name Ability
 
 
 @export var corruption := 0
-@export var action := &""
-
-@onready var player: Player = owner
 
 
-func _ready() -> void:
-	set_process_input(false) # Disable
-
-
-func _process(delta: float) -> void:
-	if _trigger():
-		pass
-
-
-func _trigger() -> bool:
-	return Input.is_action_just_pressed(action)
+func execute(object):
+	pass
