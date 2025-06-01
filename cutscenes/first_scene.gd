b2112b7664090c4f7e09a91449e2015ec248dca0
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-06-01 20:12:44 +0600

    lol

---
+++ cutscenes/first_scene.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+var dialogue:Resource = preload("res://dialogue/first_scene_dream.dialogue")
+
+func _ready() -> void:
+	DialogueManager.show_dialogue_balloon(dialogue)
+
+func change_scene_to_world():
+	get_tree().change_scene_to_file("res://world/world.tscn")
