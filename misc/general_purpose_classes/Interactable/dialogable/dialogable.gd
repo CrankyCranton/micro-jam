a84d4b13ecd956e322cdd047ff6ee2e69b301d5a
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-05-31 20:14:13 -0400

    Cut off player velocity on dialogue

--- misc/general_purpose_classes/Interactable/dialogable/dialogable.gd
+++ misc/general_purpose_classes/Interactable/dialogable/dialogable.gd
@@ -9,5 +9,6 @@ func _init() -> void:
 	Interact = func(player: Player) -> void:
 		InteractionManager.start_dialogue(dialogue, title)
 		player.set_enabled(false)
+		player.velocity = Vector2.ZERO
 		await DialogueManager.dialogue_ended
 		player.set_enabled(true)
