7b16970185f9b910ac3167e41365c85f48f0227d
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-06-05 00:53:14 +0600

    menus

--- misc/menus/pause/pause_menu.gd
+++ misc/menus/pause/pause_menu.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	hide()
 
 func pause():
+	show()
 	$AnimationPlayer.play("blur")
 	get_tree().paused = true
 
@@ -15,6 +16,14 @@ func unpause():
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("pause"):
-		pause()
-	if Input.is_action_just_pressed("pause") and get_tree().paused == true:
-		unpause()
+		if get_tree().paused == false:
+			pause()
+		elif get_tree().paused == true:
+			unpause()
+
+func _on_resume_pressed() -> void:
+	unpause()
+
+func _on_quit_pressed() -> void:
+	unpause()
+	get_tree().change_scene_to_file("res://misc/menus/main/main_menu.tscn")
