4b2aac0a5e530277f4cf35b3a54c144c350f62e7
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-06-06 14:06:17 -0400

    Cutscen SFX

--- main.gd
+++ main.gd
@@ -8,4 +8,8 @@ class_name Main extends Node
 
 
 func _on_video_finished() -> void:
+	pass#get_tree().change_scene_to_file("res://world/world.tscn")
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	get_tree().change_scene_to_file("res://world/world.tscn")
