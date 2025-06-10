47e4a5f0e1745eac9e64b761b766ebb69908346c
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-06-09 22:42:15 -0400

    Charge animation

--- player/abilities/wand/wand.gd
+++ player/abilities/wand/wand.gd
@@ -1,24 +1,50 @@
 class_name Wand extends Ability
 
+@export_group("animate")
+@export var speed := 0
+@export var damage := 0
+@export var laser := false
 
 @onready var tip: Marker2D = $Tip
 @onready var cooldown: Timer = $Cooldown
+@onready var explode_delay: Timer = $ExplodeDelay
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
-func _process(_delta: float) -> void:
-	tip.position.x = abs(tip.position.x) * player.last_direction
-	if Input.is_action_pressed(&"zap") and cooldown.is_stopped():
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"zap") and cooldown.is_stopped():
+		animation_player.play(&"charge")
+	if event.is_action_released(&"zap"):
 		_execute()
 
 
-func _input(_event: InputEvent) -> void:
-	pass
-
-
 func _execute() -> void:
+	explode_delay.stop()
+	transform.x.x = player.last_direction
+
+	if laser:
+		const LASER := preload()
+	else:
+		const BULLET := preload("res://player/abilities/wand/bullet/bullet.tscn")
+		var bullet: Area2D = BULLET.instantiate()
+		bullet.direction = player.last_direction
+		bullet.position = tip.global_position
+		get_tree().current_scene.add_child(bullet)
+
 	cooldown.start()
-	var BULLET := preload("res://player/abilities/wand/bullet/bullet.tscn")
-	var bullet: Area2D = BULLET.instantiate()
-	bullet.direction = player.last_direction
-	bullet.position = tip.global_position
-	get_tree().current_scene.add_child(bullet)
+	animation_player.play(&"RESET")
+
+
+func start_explode_chance(time_range: float) -> void:
+	const CHANCE := 0.25
+	if randf() <= CHANCE:
+		var time := randf_range(0.0, time_range)
+		explode_delay.start(time)
+
+
+func explode() -> void:
+	const EXPLOSION := preload()
+
+
+func _on_explode_delay_timeout() -> void:
+	explode()
