66c21c73214e1e888ff9a0270895ba6dce7438ab
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-06-10 23:17:22 -0400

    Contains bugs

--- player/abilities/sacrifice/sacrifice.gd
+++ player/abilities/sacrifice/sacrifice.gd
@@ -2,5 +2,5 @@ class_name Sacrifice extends Ability
 
 
 func _execute() -> void:
-	player.corruption += player.max_health - player.health
-	player.health = player.max_health
+	player.corruption += player.hit_box.max_health - player.hit_box.health
+	player.hit_box.health = player.hit_box.max_health
