4fd3928d4b42d67b2332641b80eaa04655863eb0
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-08-06 21:15:14 +0600

    what de helly

--- spirits/bosses/bullet/spawner.gd
+++ spirits/bosses/bullet/spawner.gd
@@ -1,31 +1,40 @@
 class_name Spawner extends Node2D
 
 
-@export var loaded_bullet:PackedScene
+@export var bullet_scene:PackedScene
+@onready var shoot_timer = $ShootTimer
 
-@export_group("bullet_characteristics")
-@export var number_of_bullets:int
-@export var total_spread_degrees:int
+@export_group("bullet")
+@export var rotate_speed = 100
+@export var shoot_timer_wait_time = 0.2
 
-@export_group("spawner_characteristics")
-@export var rotate_speed:int
+@export_group("spawning")
+@export var spawn_point_count = 4
+@export var radius = 100
 
-func _ready() -> void:
-	spawn()
+func _ready():	
+	var step = 2 * PI / spawn_point_count
+	
+	for i in range(spawn_point_count):
+		var spawn_point = Node2D.new()
+		var pos = Vector2(radius, 0).rotated(step * i)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		add_child(spawn_point)
+		
+	shoot_timer.wait_time = shoot_timer_wait_time
+	shoot_timer.start()
 
-func _process(delta: float) -> void:
+
+func _process(delta):
 	var new_rotation = rotation_degrees + rotate_speed * delta
 	rotation_degrees = fmod(new_rotation, 360)
+	
 
-	if Input.is_action_just_pressed("ui_accept"):
-		spawn()
-
-func spawn():
-	for i in number_of_bullets:
-		print(i)
-		var bullet:Area2D = loaded_bullet.instantiate()
-		get_parent().call_deferred("add_child",bullet)
-
-		var angle:int = total_spread_degrees/number_of_bullets
-		bullet.global_rotation_degrees = (global_rotation_degrees + angle * i) - (number_of_bullets * 8)
-		bullet.global_position = global_position.rotated(deg_to_rad(angle))
+func _on_ShootTimer_timeout() -> void:
+	for s in get_children():
+		var bullet = bullet_scene.instance()
+		get_tree().root.add_child(bullet)
+		bullet.position = s.global_position
+		bullet.rotation = s.global_rotation	
+		
